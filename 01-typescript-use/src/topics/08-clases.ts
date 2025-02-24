67b861bbea28c967a3d1f8e79fe9a4ab6853d79a
Andres Godoy <andres_amilcargg@hotmail.com>
2025-02-23 21:38:47 -0700

    Avance typescript

---
+++ 01-typescript-use/src/topics/08-clases.ts
@@ -0,0 +1,45 @@
+
+
+export class Person {
+    // public name: string;
+    // public address: string;
+
+    // constructor(  name: string, address: string ) {
+    //     this.name = name;
+    //     this.address = address;
+    // }
+
+    constructor(
+        public firstName: string,
+        public lastName: string,
+        public address: string = 'No Address'
+    ) {}
+}
+
+// export class Hero extends Person {
+
+    
+
+//     constructor(
+//         public alterEgo: string,
+//         public age: number,
+//         public realName: string
+//     ) {
+//         super(realName, 'New York')
+//     }
+// }
+export class Hero {
+
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+        public person: Person
+    ) {}
+}
+
+const person = new Person('Tony', 'Stark', 'New York')
+const ironman = new Hero('Ironman', 45, 'Tony', person);
+
+console.log(ironman)
+
