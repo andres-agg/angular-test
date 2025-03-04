6ff28052cbe4117ee86c9b7d9a110b0f814c1490
Andres Godoy <andres.gonzalez@coppel.com>
2025-03-04 16:51:37 -0700

    Seccion 6 componentes base

--- 03-gifs-app/src/app/gifs/components/side-menu-header/side-menu-header.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'gifs-side-menu-header',
-  imports: [],
-  templateUrl: './side-menu-header.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class SideMenuHeaderComponent { }
